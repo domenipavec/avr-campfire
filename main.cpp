2405f7d8ab91f1d2b308d6d5bf7fb0f6cb6c6dc8
matematik7 <domen.ipavec@z-v.si>
2013-10-10 18:17:47 +0200

    Changed pinout to pcb

--- main.cpp
+++ main.cpp
@@ -38,63 +38,83 @@
 #include "pwm.h"
 #include "random32.h"
 
-#define RED_START 0
-#define YELLOW_START 0
+#define RED_START 10
+#define YELLOW_START 10
 
 avr_cpp_lib::pwm_channel pwm_data[30] = {
 	// rdece ledice
-	{&DDRB, &PORTB, PB4, RED_START}, // R4
-	{&DDRA, &PORTA, PA2, RED_START}, // R5
-	{&DDRA, &PORTA, PA3, RED_START}, // R1
-	{&DDRA, &PORTA, PA5, RED_START}, // R6
-	{&DDRA, &PORTA, PA7, RED_START}, // R2
-	{&DDRC, &PORTC, PC6, RED_START}, // R7
-	{&DDRB, &PORTB, PB0, RED_START}, // R8
-	{&DDRB, &PORTB, PB1, RED_START}, // R12
-	{&DDRD, &PORTD, PD7, RED_START}, // R11
-	{&DDRD, &PORTD, PD5, RED_START}, // R3
-	{&DDRD, &PORTD, PD3, RED_START}, // R10
-	{&DDRD, &PORTD, PD1, RED_START}, // R9
+	{&DDRB, &PORTB, PB3, RED_START}, // R4
+	{&DDRB, &PORTB, PB5, RED_START}, // R5
+	{&DDRB, &PORTB, PB6, RED_START}, // R1
+	{&DDRD, &PORTD, PD0, RED_START}, // R6
+	{&DDRD, &PORTD, PD2, RED_START}, // R2
+	{&DDRD, &PORTD, PD4, RED_START}, // R7
+	{&DDRD, &PORTD, PD6, RED_START}, // R8
+	{&DDRA, &PORTA, PA6, RED_START}, // R12
+	{&DDRC, &PORTC, PC6, RED_START}, // R11
+	{&DDRC, &PORTC, PC4, RED_START}, // R3
+	{&DDRC, &PORTC, PC2, RED_START}, // R10
+	{&DDRC, &PORTC, PC0, RED_START}, // R9
 
 	// rumene ledice
-	{&DDRB, &PORTB, PB3, YELLOW_START}, // Y4
-	{&DDRA, &PORTA, PA1, YELLOW_START}, // Y5
-	{&DDRC, &PORTC, PC0, YELLOW_START}, // Y1
-	{&DDRA, &PORTA, PA4, YELLOW_START}, // Y6
-	{&DDRA, &PORTA, PA6, YELLOW_START}, // Y2
-	{&DDRC, &PORTC, PC7, YELLOW_START}, // Y7
-	{&DDRB, &PORTB, PB2, YELLOW_START}, // Y8
-	{&DDRC, &PORTC, PC1, YELLOW_START}, // Y12
-	{&DDRD, &PORTD, PD6, YELLOW_START}, // Y11
-	{&DDRD, &PORTD, PD4, YELLOW_START}, // Y3
-	{&DDRD, &PORTD, PD2, YELLOW_START}, // Y10
-	{&DDRD, &PORTD, PD0, YELLOW_START}, // Y9
+	{&DDRA, &PORTA, PA5, YELLOW_START}, // Y4
+	{&DDRB, &PORTB, PB4, YELLOW_START}, // Y5
+	{&DDRC, &PORTC, PC7, YELLOW_START}, // Y1
+	{&DDRB, &PORTB, PB7, YELLOW_START}, // Y6
+	{&DDRD, &PORTD, PD1, YELLOW_START}, // Y2
+	{&DDRD, &PORTD, PD3, YELLOW_START}, // Y7
+	{&DDRD, &PORTD, PD5, YELLOW_START}, // Y8
+	{&DDRA, &PORTA, PA7, YELLOW_START}, // Y12
+	{&DDRC, &PORTC, PC5, YELLOW_START}, // Y11
+	{&DDRC, &PORTC, PC3, YELLOW_START}, // Y3
+	{&DDRC, &PORTC, PC1, YELLOW_START}, // Y10
+	{&DDRD, &PORTD, PD7, YELLOW_START}, // Y9
 
 	PWM_CHANNEL_END
 };
 	
 avr_cpp_lib::pwm_worker pwm(pwm_data);
 
-void update_values() {
-	//	for (uint8_t x = 0; x < 24; x++) {
-	static uint8_t x = 0;
-	static uint8_t intensity = 0;
-	intensity++;
-	if (intensity == 0) {
-		x ++;
-		if (x == 12) {
-			x = 0;
-		}
-	}
-
-	uint8_t color = 100;
-
+void setLed(uint8_t x, uint8_t intensity, uint8_t color) {
 	uint16_t tmp = color * intensity;
 	tmp /= 255;
 
 	pwm_data[x].value = tmp;
 	pwm_data[x+12].value = intensity - tmp;
-	//}
+}
+
+#define INT_START 4
+
+uint8_t intensities[12] = {INT_START, INT_START, INT_START, INT_START, INT_START, INT_START,INT_START, INT_START, INT_START, INT_START, INT_START, INT_START};
+
+void update_values() {
+	for (uint8_t x = 0; x < 12; x++) {
+		uint8_t color = 100;
+		if (color < 10) {
+			color = 10;
+		}
+		if (color > 245) {
+			color = 245;
+		}
+		color -= 10;
+		color += get_random32(21);
+		
+		uint8_t a = get_random32(2);
+		if (a == 0) {
+			if (intensities[x] < 128) {
+				intensities[x] <<= 1;
+			}
+		} else  if (a == 1){
+			if (intensities[x] > INT_START) {
+				intensities[x] >>= 1;
+			}
+		}
+
+		uint8_t lightness = ADCH;
+		uint16_t intensity = intensities[x] * lightness;
+		
+		setLed(x, intensity >> 7, color);
+	}
 }
 
 int main() {
@@ -104,12 +124,17 @@ int main() {
 	// enable interrupts
 	//sei();
 
+	// adc settings
+	ADMUX = 0b01100000;
+	ADCSRA = 0b11100111;
+
 
 	// count pwm cycle
 	uint8_t i = 0;
-	uint16_t c = 100;
+	uint16_t c = 5120;
 
 	update_values();
+
 
 	for (;;) {
 
@@ -118,9 +143,9 @@ int main() {
 
 		c--;
 		if (c== 0) {
+			uint8_t speed = 100;
 			update_values();
-			//c = get_random32(5)*1000 + 5000;
-			c = 100;
+			c = get_random32(speed>>3)*256 + 128*(speed>>1) + 1024;
 		}
 
 	}
